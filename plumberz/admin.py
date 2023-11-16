e79835288fd3dba21dd4ad990eb69bf7435b346b
mradebowale <hamzwalex@gmail.com>
2023-11-16 10:44:55 +0100

    update

--- plumberz/admin.py
+++ plumberz/admin.py
@@ -3,8 +3,8 @@ from .models import Booking, New, contactform
 # Register your models here.
 
 class ContactAdmin(admin.ModelAdmin):
-    list_display = ["name", "email", "subject"]
-    list_filter = ["name", "email", "subject"]
+    list_display = ["name", "email", "type"]
+    list_filter = ["name", "email", "type"]
     search_fields = ["name"]
 
 admin.site.register(Booking, ContactAdmin)
@@ -19,9 +19,9 @@ admin.site.register(New, NewsAdmin)
 
 
 class ContactAdmin2(admin.ModelAdmin):
-    list_display = ["name", "email", "subject"]
-    list_filter = ["name", "email", "subject"]
-    search_fields = ["name"]
+    list_display = ["names", "emails", "services"]
+    list_filter = ["names", "emails", "services"]
+    search_fields = ["names"]
 
 
 admin.site.register(contactform, ContactAdmin2)
