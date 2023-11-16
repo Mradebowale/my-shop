81a57c4fe1d9e36bf186daadc5199b76cc452b98
mradebowale <hamzwalex@gmail.com>
2023-11-16 10:41:08 +0100

    updated

--- plumberz/admin.py
+++ plumberz/admin.py
@@ -2,7 +2,12 @@ from django.contrib import admin
 from .models import Booking, New, contactform
 # Register your models here.
 
-admin.site.register([Booking, contactform])
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ["name", "email", "subject"]
+    list_filter = ["name", "email", "subject"]
+    search_fields = ["name"]
+
+admin.site.register(Booking, ContactAdmin)
 
 class NewsAdmin(admin.ModelAdmin):
     list_display = ["Headline"]
@@ -12,3 +17,11 @@ class NewsAdmin(admin.ModelAdmin):
 
 admin.site.register(New, NewsAdmin)
 
+
+class ContactAdmin2(admin.ModelAdmin):
+    list_display = ["name", "email", "subject"]
+    list_filter = ["name", "email", "subject"]
+    search_fields = ["name"]
+
+
+admin.site.register(contactform, ContactAdmin2)
